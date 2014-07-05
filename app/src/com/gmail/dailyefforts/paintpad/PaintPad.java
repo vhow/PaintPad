01fb32333467095235e10632861e1e4189037c70
DailyEfforts <DailyEfforts@gmail.com>
2014-07-05 14:59:21 +0900

    Save png file to public `Picture` dir.

--- app/src/com/gmail/dailyefforts/paintpad/PaintPad.java
+++ app/src/com/gmail/dailyefforts/paintpad/PaintPad.java
@@ -1,5 +1,21 @@
 package com.gmail.dailyefforts.paintpad;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.os.Environment;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.gmail.dailyefforts.paintpad.config.Config;
+import com.gmail.dailyefforts.paintpad.drawings.Drawing;
+import com.gmail.dailyefforts.paintpad.drawings.Eraser;
+import com.gmail.dailyefforts.paintpad.helper.ScreenInfo;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -8,232 +24,202 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.os.Environment;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Toast;
-
-import com.gmail.dailyefforts.paintpad.drawings.Drawing;
-import com.gmail.dailyefforts.paintpad.drawings.Eraser;
-import com.gmail.dailyefforts.paintpad.helper.ScreenInfo;
-
 /**
  * This is the main View class.
  */
 public class PaintPad extends View {
-	private static final String TIME_FORMAT = "yyyy-MM-dd_HH.mm.ss";
-	private static final String SUFFIX_NAME = ".png";
-	private float mX;
-	private float mY;
+    private static final String TAG = PaintPad.class.getSimpleName();
 
-	private Bitmap mBitmap;
-	private Canvas mCanvas;
+    private float mX;
+    private float mY;
 
-	private boolean mIsMoving;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
 
-	private Drawing mDrawing;
+    private boolean mIsMoving;
 
-	private Context mContext;
-	private Bitmap mPen;
+    private Drawing mDrawing;
 
-	/**
-	 * Set the shape that is drawing.
-	 * 
-	 * @param drawing
-	 *            Which shape to drawing current.
-	 */
-	public void setDrawing(Drawing drawing) {
-		mDrawing = drawing;
-	}
+    private Context mContext;
+    private Bitmap mPen;
 
-	public PaintPad(Context context) {
-		super(context);
-		mContext = context;
+    /**
+     * Set the shape that is drawing.
+     *
+     * @param drawing Which shape to drawing current.
+     */
+    public void setDrawing(Drawing drawing) {
+        mDrawing = drawing;
+    }
 
-		// Get the information about the screen.
-		final ScreenInfo screenInfo = new ScreenInfo((Activity) context);
+    public PaintPad(Context context) {
+        super(context);
+        mContext = context;
 
-		/**
-		 * Create a bitmap with the size of the screen.
-		 */
-		mBitmap = Bitmap.createBitmap(screenInfo.getWidthPixels(),
-				screenInfo.getHeightPixels(), Bitmap.Config.ARGB_8888);
+        // Get the information about the screen.
+        final ScreenInfo screenInfo = new ScreenInfo((Activity) context);
 
-		mCanvas = new Canvas(mBitmap);
+        /**
+         * Create a bitmap with the size of the screen.
+         */
+        mBitmap = Bitmap.createBitmap(screenInfo.getWidthPixels(),
+                screenInfo.getHeightPixels(), Bitmap.Config.ARGB_8888);
 
-		// Set the background color
-		mCanvas.drawColor(getResources().getColor(R.color.color_default_bg));
+        mCanvas = new Canvas(mBitmap);
 
-		mIsMoving = false;
+        // Set the background color
+        mCanvas.drawColor(getResources().getColor(R.color.color_default_bg));
 
-		mPen = BitmapFactory.decodeResource(getResources(), R.drawable.pen);
-	}
+        mIsMoving = false;
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
+        mPen = BitmapFactory.decodeResource(getResources(), R.drawable.pen);
+    }
 
-		canvas.drawBitmap(mBitmap, 0, 0, null);
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
-		// Call the drawing's draw() method.
-		if (mDrawing != null && mIsMoving == true) {
-			mDrawing.draw(canvas);
-		}
+        canvas.drawBitmap(mBitmap, 0, 0, null);
 
-		if (!(mDrawing instanceof Eraser)) {
-			canvas.drawBitmap(mPen, mX, mY - mPen.getHeight(), null);
-		}
-	}
-	
-	@Override
-	public boolean performClick() {
-		return super.performClick();
-	}
+        // Call the drawing's draw() method.
+        if (mDrawing != null && mIsMoving == true) {
+            mDrawing.draw(canvas);
+        }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		final float x = event.getX();
-		final float y = event.getY();
+        if (!(mDrawing instanceof Eraser)) {
+            canvas.drawBitmap(mPen, mX, mY - mPen.getHeight(), null);
+        }
+    }
 
-		final int action = event.getAction() & MotionEvent.ACTION_MASK;
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
 
-		switch (action) {
-		case MotionEvent.ACTION_DOWN:
-			fingerDown(x, y);
-			break;
-		case MotionEvent.ACTION_MOVE:
-			fingerMove(x, y);
-			break;
-		case MotionEvent.ACTION_UP:
-			fingerUp(x, y);
-			break;
-		default:
-			break;
-		}
-		invalidate();
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        final float x = event.getX();
+        final float y = event.getY();
 
-		return true;
-	}
+        final int action = event.getAction() & MotionEvent.ACTION_MASK;
 
-	/**
-	 * Handles the action of finger up.
-	 * 
-	 * @param x
-	 *            coordinate
-	 * @param y
-	 *            coordinate
-	 */
-	private void fingerUp(float x, float y) {
-		mX = 0;
-		mY = 0;
-		mDrawing.fingerUp(x, y, mCanvas);
-		mIsMoving = false;
-	}
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                fingerDown(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                fingerMove(x, y);
+                break;
+            case MotionEvent.ACTION_UP:
+                fingerUp(x, y);
+                break;
+            default:
+                break;
+        }
+        invalidate();
 
-	/**
-	 * Handles the action of finger Move.
-	 * 
-	 * @param x
-	 *            coordinate
-	 * @param y
-	 *            coordinate
-	 */
-	private void fingerMove(float x, float y) {
-		mX = x;
-		mY = y;
-		mIsMoving = true;
-		mDrawing.fingerMove(x, y, mCanvas);
-	}
+        return true;
+    }
 
-	/**
-	 * Handles the action of finger down.
-	 * 
-	 * @param x
-	 *            coordinate
-	 * @param y
-	 *            coordinate
-	 */
-	private void fingerDown(float x, float y) {
-		mIsMoving = false;
-		mDrawing.fingerDown(x, y, mCanvas);
-	}
+    /**
+     * Handles the action of finger up.
+     *
+     * @param x coordinate
+     * @param y coordinate
+     */
+    private void fingerUp(float x, float y) {
+        mX = 0;
+        mY = 0;
+        mDrawing.fingerUp(x, y, mCanvas);
+        mIsMoving = false;
+    }
 
-	/**
-	 * Check the sdcard is available or not.
-	 */
-	public void saveBitmap() {
-		String state = Environment.getExternalStorageState();
-		if (Environment.MEDIA_MOUNTED.equals(state)) {
-			saveToSdcard();
-		} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-			Toast.makeText(mContext,
-					getResources().getString(R.string.tip_sdcard_is_read_only),
-					Toast.LENGTH_LONG).show();
-		} else {
-			Toast.makeText(
-					mContext,
-					getResources().getString(
-							R.string.tip_sdcard_is_not_available),
-					Toast.LENGTH_SHORT).show();
-		}
-	}
+    /**
+     * Handles the action of finger Move.
+     *
+     * @param x coordinate
+     * @param y coordinate
+     */
+    private void fingerMove(float x, float y) {
+        mX = x;
+        mY = y;
+        mIsMoving = true;
+        mDrawing.fingerMove(x, y, mCanvas);
+    }
 
-	public void changeBgColor(int color) {
-		mCanvas.drawColor(color);
-		invalidate();
-	}
+    /**
+     * Handles the action of finger down.
+     *
+     * @param x coordinate
+     * @param y coordinate
+     */
+    private void fingerDown(float x, float y) {
+        mIsMoving = false;
+        mDrawing.fingerDown(x, y, mCanvas);
+    }
 
-	/**
-	 * Clear the drawing in the canvas.
-	 */
-	public void clearCanvas() {
-		mCanvas.drawColor(getResources().getColor(R.color.color_default_bg));
-		invalidate();
-	}
+    /**
+     * Check the sdcard is available or not.
+     */
+    public void saveBitmap() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            saveToSdcard();
+        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            Toast.makeText(mContext,
+                    getResources().getString(R.string.tip_sdcard_is_read_only),
+                    Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(
+                    mContext,
+                    getResources().getString(
+                            R.string.tip_sdcard_is_not_available),
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+    }
 
-	/**
-	 * Save the bitmap to sdcard.
-	 */
-	private void saveToSdcard() {
-		final File sdcard_path = Environment.getExternalStorageDirectory();
-		String myFloder = getResources().getString(
-				R.string.folder_name_in_sdcard);
-		File paintpad = new File(sdcard_path + "/" + myFloder + "/");
-		// TODO use public pic dir
-		try {
-			if (!paintpad.exists()) {
-				paintpad.mkdirs();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    public void changeBgColor(int color) {
+        mCanvas.drawColor(color);
+        invalidate();
+    }
 
-		final SimpleDateFormat format = new SimpleDateFormat(TIME_FORMAT,
-				Locale.getDefault());
-		final Date date = Calendar.getInstance().getTime();
+    /**
+     * Clear the drawing in the canvas.
+     */
+    public void clearCanvas() {
+        mCanvas.drawColor(getResources().getColor(R.color.color_default_bg));
+        invalidate();
+    }
 
-		// Get formatted time stamp
-		String timeStamp = format.format(date);
+    /**
+     * Save the bitmap to sdcard.
+     */
+    private void saveToSdcard() {
+        final File picFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        if (!picFolder.exists()) {
+            picFolder.mkdirs();
+        }
 
-		String fullPath = sdcard_path + File.separator + myFloder
-				+ File.separator + timeStamp + SUFFIX_NAME;
-		try {
-			Toast.makeText(mContext,
-					getResources().getString(R.string.tip_save_to) + fullPath,
-					Toast.LENGTH_LONG).show();
-			mBitmap.compress(Bitmap.CompressFormat.PNG, 100,
-					new FileOutputStream(fullPath));
-		} catch (FileNotFoundException e) {
-			Toast.makeText(
-					mContext,
-					getResources().getString(R.string.tip_sava_failed)
-							+ fullPath, Toast.LENGTH_LONG).show();
-			e.printStackTrace();
-		}
-	}
+        final SimpleDateFormat format = new SimpleDateFormat(Config.TIME_FORMAT,
+                Locale.getDefault());
+        final Date date = Calendar.getInstance().getTime();
+
+        final String timeStamp = format.format(date);
+        final String fileName = timeStamp + Config.SUFFIX_NAME;
+        final File pngFile = new File(picFolder, fileName);
+        try {
+            mBitmap.compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(pngFile));
+            Toast.makeText(getContext(),
+                    getResources().getString(R.string.tip_save_to) + pngFile.getAbsolutePath(),
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            Toast.makeText(
+                    mContext,
+                    getResources().getString(R.string.tip_sava_failed)
+                            + fileName, Toast.LENGTH_LONG
+            ).show();
+            Log.e(TAG, e.getMessage());
+        }
+    }
 }
---
+++ app/src/com/gmail/dailyefforts/paintpad/config/Config.java
@@ -0,0 +1,8 @@
+package com.gmail.dailyefforts.paintpad.config;
+
+public class Config {
+    public static final String TIME_FORMAT = "yyyy-MM-dd_HH.mm.ss";
+    public static final String SUFFIX_NAME = ".png";
+    private Config() {
+    }
+}
